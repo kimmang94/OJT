e8296482af4d41500c1a9c6cc96a0143e74dc345
kimhyeok <kHyeok@salin.co.kr>
2023-03-02 11:23:52 +0900

    define Function

--- OJT Project/Assets/Scripts/Pet.cs
+++ OJT Project/Assets/Scripts/Pet.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pet : MonoBehaviour
+public class Pet : Player
 {
     Transform playerTransform;
 
--- OJT Project/Assets/Scripts/Wood.cs
+++ OJT Project/Assets/Scripts/Wood.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wood : MonoBehaviour
+public class Wood : Player
 {
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void PlayerLook()

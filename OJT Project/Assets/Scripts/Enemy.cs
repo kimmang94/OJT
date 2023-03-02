9ca71a01c1c20a5dcc21ae330be07fee68298182
kimhyeok <kHyeok@salin.co.kr>
2023-03-02 16:39:42 +0900

    add file

--- OJT Project/Assets/Scripts/Enemy.cs
+++ OJT Project/Assets/Scripts/Enemy.cs
@@ -4,26 +4,22 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    bool isHit = false;
     
     void Update()
     {
-        HitAttack();
-    }
-
-    public void HitAttack()
-    {
-        // 총알과 맞았는지 확인
-        if (false)
-        {
-            DestroyEnemy();
-        }
         
     }
 
-    public void DestroyEnemy()
+
+
+    public void HitCheckAndDestroyEnemy()
     {
-        // 충돌 확인후 본인 제거
-        StartCoroutine(DestroyAfterFiveSecound());
+        if (isHit)
+        {
+            // 충돌 확인후 본인 제거
+            StartCoroutine(DestroyAfterFiveSecound());
+        }
     }
 
     IEnumerator DestroyAfterFiveSecound()

2b5957e983f75ce603018e07941b92975d049a9a
kimhyeok <kHyeok@salin.co.kr>
2023-03-07 10:54:16 +0900

    add Resources Manager

--- OJT Project/Assets/Scripts/Pet.cs
+++ OJT Project/Assets/Scripts/Pet.cs
@@ -4,24 +4,5 @@ using UnityEngine;
 
 public class Pet : MonoBehaviour
 {
-    Player playerTransform;
-    Transform target;
-
-    Vector3 direction;
-    private void OnEnable()
-    {
-        playerTransform = GetComponent<Player>();
-        GetPlayerTransform();
-    }
-
-    private void Update()
-    {
-        transform.LookAt(target);
-    }
-    void GetPlayerTransform()
-    {
-        target = playerTransform.transform;
-        
-    }
-
+    
 }
